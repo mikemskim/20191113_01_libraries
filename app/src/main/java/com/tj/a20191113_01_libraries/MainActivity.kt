d305ae1595a732dbf51e2711c2279a0ff70ecb73
mikemskim <mskim@steconsulting.co.kr>
2019-11-13 19:50:37 +0900

    글라이드를 이용해서 이미지 로딩 예제

--- app/src/main/java/com/tj/a20191113_01_libraries/MainActivity.kt
+++ app/src/main/java/com/tj/a20191113_01_libraries/MainActivity.kt
@@ -2,6 +2,8 @@ package com.tj.a20191113_01_libraries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -18,5 +20,7 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        var url = "https://ppss.kr/wp-content/uploads/2016/11/album-540x540.jpg"
+        Glide.with(mContext).load(url).into(mainCirImg)
     }
 }
